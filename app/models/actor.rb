986b27efda8b5c2066107a29e6436911823de77c
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-03-26 19:42:56 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,9 +1,10 @@
+require 'pry'
 class Actor < ActiveRecord::Base
   has_many :characters
   has_many :shows, through: :characters
 
   def full_name
-    self.first_name self.last_name
+    binding.pry
   end
 
   def list_roles
