961881268daaae1e55d0db358dfca0430d454067
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-03-26 19:47:15 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -10,7 +10,7 @@ class Actor < ActiveRecord::Base
   def list_roles
     roles = []
     self.characters.each do |character|
-      roles << "#{character} - #{character.show}"
+      roles << "#{character.name} - #{character.show.name}"
     end
     roles
   end
